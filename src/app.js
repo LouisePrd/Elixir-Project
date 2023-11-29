a163e19e55a65fe307e11013f5db629ecec4c9e2
LouisePrd <louise.peridy22@gmail.com>
2023-11-29 20:27:05 +0100

    test API random cocktail

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 let request = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
+let randomRequest = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-fetch(request).then((response) => {
+/*fetch(request).then((response) => {
     console.log(response);
     response.json().then((data) => {
         console.log(data);
     });
+});*/
+
+fetch(randomRequest).then((response) => {
+  response.json().then((data) => {
+    getInfo(data);
+  });
 });
+
+function getInfo(data) {
+  let name = document.getElementById("nameCocktail");
+  name.innerHTML = data.drinks[0].strDrink;
+
+  let tabIngredients = [];
+  for (let i = 1; i <= 5; i++) {
+    let ingredient = data.drinks[0]["strIngredient" + i];
+    if (ingredient != null) {
+      tabIngredients.push(ingredient);
+      console.log(ingredient);
+    }
+  }
+}
