b92a912b7d2d69321ac2e5d2b81a8ab3c60a03ee
LouisePrd <louise.peridy22@gmail.com>
2023-12-04 16:50:31 +0100

    clean + add/delete functions

--- src/app.js
+++ src/app.js
@@ -1,64 +1,79 @@
-let request =
-  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
-let randomCocktail = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+let randomRequest =
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=" +
+  String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 let tabIngredients = [];
 let tabAllIngredients = [];
 
 // API request to get the cocktail
-fetch(randomCocktail).then((response) => {
-  response.json().then((data) => {
-    getInfo(data);
-  });
-});
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php").then(
+  (response) => {
+    response.json().then((data) => {
+      getInfo(data);
+    });
+  }
+);
 
-// We get the name of the cocktail and the first 5 ingredients
+// Get the name of the cocktail and the first 5 ingredients
 function getInfo(data) {
   let name = document.getElementById("nameCocktail");
   name.innerHTML = data.drinks[0].strDrink;
   for (let i = 1; i <= 5; i++) {
     let ingredient = data.drinks[0]["strIngredient" + i];
-    if (ingredient != null) {
+    if (ingredient != null && data.drinks != null) {
       tabIngredients.push(ingredient);
     }
   }
   createListIngredients();
 }
 
-// We create a list of random ingredients so the user can have options
-// We want 12 ingredients in total, 5 of them are the ingredients of the main cocktail
+// Create a list of random ingredients for user options, 12 ingredients in total
 function createListIngredients() {
-  let randomLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  let randomRequest =
-    "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=" + randomLetter;
+  tabAllIngredients = tabAllIngredients.concat(tabIngredients);
+
   fetch(randomRequest).then((response) => {
     response.json().then((data) => {
-      if (
-        data.drinks.length > 12 - tabIngredients.length 
-      ) {
-        for (let i = 0; i < 12 - tabIngredients.length; i++) {
-          if (
-            !tabIngredients.includes(data.drinks[i].strIngredient1) &&
-            !tabAllIngredients.includes(data.drinks[i].strIngredient1)
-          )
-            tabAllIngredients.push(data.drinks[i].strIngredient1);
-        }
+      if (data.drinks.length > 12 - tabIngredients.length) {
       } else createListIngredients();
     });
   });
 
+  // Wait 100ms to be sure that the list is created
   setTimeout(function () {
     displayIngredients();
   }, 100);
 }
 
-// We display the ingredients in the HTML to create a list
+// Display the ingredients in the HTML to create a list
 function displayIngredients() {
-  tabAllIngredients = tabAllIngredients.concat(tabIngredients);
   let listIngredients = document.getElementById("listIngredients");
-  listIngredients.innerHTML = "";
   for (let i = 0; i < tabAllIngredients.length; i++) {
     let li = document.createElement("li");
     li.innerHTML = tabAllIngredients[i];
     listIngredients.appendChild(li);
+
+    li.addEventListener("click", function () {
+      addIngredient(tabAllIngredients[i]);
+    });
+  }
+}
+
+// Add an ingredient to the user recipe
+function addIngredient(ingredient) {
+  let choice = document.getElementById("recipe");
+  let li = document.createElement("li");
+  li.innerHTML = ingredient;
+  choice.appendChild(li);
+  li.addEventListener("click", function () {
+    deleteIngredient(ingredient);
+  });
+}
+
+// Delete an ingredient from the user recipe
+function deleteIngredient(ingredient) {
+  let place = document.getElementById("recipe").getElementsByTagName("li");
+  for (let i = 0; i < place.length; i++) {
+    if (place[i].innerHTML == ingredient) {
+      place[i].remove();
+    }
   }
 }
